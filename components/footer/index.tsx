eafb219f0a82d49def6e2b5aa3057d18810768ff
Manuel Selch <manuel@selch.de>
2025-11-15 17:27:31 +0100

    feat(home): layout & hero

---
+++ components/footer/index.tsx
@@ -0,0 +1,10 @@
+import { Container } from "@mantine/core";
+
+
+export default function Footer() {
+    return (
+        <Container>
+            
+        </Container>
+    )
+}
---
+++ components/header/index.tsx
@@ -0,0 +1,15 @@
+import { Container, Divider, Group, Image, Paper } from '@mantine/core';
+import logo from "@/img/logo.jpeg"
+
+export function Header() {
+  return (
+    <header>
+      <Paper bg="dark" py="md">
+        <Group justify='center'>
+            <Image src={logo.src} fit="contain" w={70} h={70}/>
+        </Group>
+        <Divider mt="md" color="gold"/>
+      </Paper>
+    </header>
+  );
+}
