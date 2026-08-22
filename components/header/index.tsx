8e94cfa1e0e73a7f6b1fc590a2c3f9f7a5d1cc59
Manuel Selch <manuel@selch.de>
2026-08-22 16:08:40 +0200

    feat(header): logo redirects to homepage

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,4 +1,4 @@
-import { Center, Image, Stack } from "@mantine/core";
+import { Box, Center, Image, Stack } from "@mantine/core";
 import logo from "@/img/logo.png";
 
 export function Header() {
@@ -6,13 +6,21 @@ export function Header() {
     <header className="sticky top-0 z-50 border-b border-transparent bg-paper/90">
       <Stack>
         <Center>
+          <Box
+            component="a"
+            href="/"
+            aria-label="Zur Auto Deko Startseite"
+            display="block"
+            style={{ lineHeight: 0 }}
+          >
             <Image
               className="h-24 w-16 object-contain drop-shadow-md"
               src={logo.src}
               alt="Auto Deko Handmade"
               fit="contain"
             />
-          </Center>
+          </Box>
+        </Center>
       </Stack>
     </header>
   );
